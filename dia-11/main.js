59dfef2c56a735b0fe1405e6fb856486db53fd59
jeffer503 <jeffersonhomero123@gmail.com>
2025-06-02 10:54:19 -0600

    subiendo mini proyecto

---
+++ dia-11/main.js
@@ -0,0 +1,68 @@
+// Este es un comentario de una sola linea
+
+/* este 
+es un 
+comentario en varias lineas*/
+
+let nombre = "jeffersson";
+
+let edad = 19;
+
+let varon = true;
+
+let bigint = 383487392794894739727n;
+
+let indefinida;
+
+let simbolo = Symbol("age19");
+
+let nulo = null;
+
+console.log(nombre);
+
+console.log(edad);
+
+console.log(varon);
+
+console.log(bigint);
+
+console.log(indefinida);
+
+console.log(simbolo);
+
+console.log(nulo);
+
+console.log(typeof nombre);
+
+console.log(typeof edad);
+
+console.log(typeof varon);
+
+console.log(typeof bigint);
+
+console.log(typeof indefinida);
+
+console.log(typeof simbolo);
+
+console.log(typeof nulo);
+
+apellido = "Melgar";
+console.log(apellido);
+
+mes = 6;
+console.log(mes);
+
+varon = false;
+console.log(varon);
+
+numeroGrande = 876986897598656788n;
+console.log(numeroGrande);
+
+simbolo = symbol("day11");
+console.log(simbolo);
+
+nulo = hola;
+console.log(nulo);
+
+indefinida;
+console.log(indefinida);
